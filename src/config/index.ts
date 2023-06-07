dd48595ed8a9453afb495a16b6bd92d57cdfa258
kunarajahnirojan <kunarajahnirojan@gmail.com>
2023-06-07 12:00:40 +0530

    changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,5 @@
-export {};
+export const lang = process.env.REACT_APP_LANG;
+export const name = process.env.REACT_APP_NAME;
+export const authURL = process.env.REACT_APP_AUTH_URL;
+export const apiURL = process.env.REACT_APP_API_URL;
+export const secretKey = process.env.REACT_APP_KEY || 'example key';
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter';
 import ticketReducer from '../features/ticket';
